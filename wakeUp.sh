e99e4465039f7b3b9a84ea75cbc88157564e0c00
abe <abdessalam.boujane@gmail.com>
2016-04-02 13:47:59 -0400

    infinite loop on one song

--- wakeUp.sh
+++ wakeUp.sh
@@ -2,58 +2,62 @@
 #AutomatedSwitch.sh
 #Navigate to my GIT directory and launch the python script and then go back to  root
 
-cd /
-cd home/pi/
+# cd /
+# cd home/pi/
+
+while true
+do
+  curl --data "v=zKGW9V1WSl8" http://10.0.0.2:8008/apps/YouTube
+  sleep 242
+done
 
 
-#curl --data "v=YykjpeuMNEk&list=PLFgquLnL59altZg1f_Kr1kGUYE6j-NE0M&resume" http://10.0.0.2:8008/apps/YouTube
-
-#Christina Aguilera - Genie In A Bottle (Bentley Grey Remix)
-echo '''#Christina Aguilera - Genie In A Bottle (Bentley Grey Remix)'''
-curl --data "v=2Nev2NEKLVM" http://10.0.0.2:8008/apps/YouTube
-sleep 4m
-sleep 29
-#Cassie - Me & You (Suprafive Remix)
-echo '''#Cassie - Me & You (Suprafive Remix)'''
-curl --data "v=Go7X8mOXR3E" http://10.0.0.2:8008/apps/YouTube
-sleep 3m
-sleep 25
-##Jay Sean - Ride it (Bentley Grey Remix)
-echo '''#Jay Sean - Ride it (Bentley Grey Remix)'''
-curl --data "v=07x2NioGw-o" http://10.0.0.2:8008/apps/YouTube
-sleep 4m
-sleep 15
-##Shift K3Y - I Know
-echo '''#Shift K3Y - I Know'''
-curl --data "v=h-nnAeByB1A" http://10.0.0.2:8008/apps/YouTube
-sleep 3m
-sleep 25
-#Sigala - Sweet Lovin' (Official Video) Feat. Bryn Christopher
-echo '''#Sigala - Sweet Lovin (Official Video) Feat. Bryn Christopher'''
-curl --data "v=qj5zT4t7S6c" http://10.0.0.2:8008/apps/YouTube
-sleep 3m
-sleep 40
-##Alan Walker - Faded
-echo '''#Alan Walker - Faded'''
-curl --data "v=60ItHLz5WEA" http://10.0.0.2:8008/apps/YouTube
-sleep 3m
-sleep 38
-##Christina Aguilera ft. P.Diddy - Tell Me (Suprafive Remix 2k15)(Music Video)
-echo '''#Christina Aguilera ft. P.Diddy - Tell Me (Suprafive Remix 2k15)(Music Video)'''
-curl --data "v=VEVPbzP9QKU" http://10.0.0.2:8008/apps/YouTube
-sleep 4m
-sleep 13
-##Duke Dumont - Ocean Drive
-echo '''#Duke Dumont - Ocean Drive'''
-curl --data "v=KDxJlW6cxRk" http://10.0.0.2:8008/apps/YouTube
-sleep 3m
-sleep 33
-#Carly Rae Jepsen - I Really Like You (Dj Denis Rublev & Dj Anton Remix 2k16)
-echo '''Carly Rae Jepsen - I Really Like You (Dj Denis Rublev & Dj Anton Remix 2k16)'''
-curl --data "v=7UALo2Fkjfc" http://10.0.0.2:8008/apps/YouTube
-sleep 4m
-sleep 55
-echo “Finished Playing List”
+# #Christina Aguilera - Genie In A Bottle (Bentley Grey Remix)
+# echo '''#Christina Aguilera - Genie In A Bottle (Bentley Grey Remix)'''
+# curl --data "v=2Nev2NEKLVM" http://10.0.0.2:8008/apps/YouTube
+# sleep 4m
+# sleep 29
+# #Cassie - Me & You (Suprafive Remix)
+# echo '''#Cassie - Me & You (Suprafive Remix)'''
+# curl --data "v=Go7X8mOXR3E" http://10.0.0.2:8008/apps/YouTube
+# sleep 3m
+# sleep 25
+# ##Jay Sean - Ride it (Bentley Grey Remix)
+# echo '''#Jay Sean - Ride it (Bentley Grey Remix)'''
+# curl --data "v=07x2NioGw-o" http://10.0.0.2:8008/apps/YouTube
+# sleep 4m
+# sleep 15
+# ##Shift K3Y - I Know
+# echo '''#Shift K3Y - I Know'''
+# curl --data "v=h-nnAeByB1A" http://10.0.0.2:8008/apps/YouTube
+# sleep 3m
+# sleep 25
+# #Sigala - Sweet Lovin' (Official Video) Feat. Bryn Christopher
+# echo '''#Sigala - Sweet Lovin (Official Video) Feat. Bryn Christopher'''
+# curl --data "v=qj5zT4t7S6c" http://10.0.0.2:8008/apps/YouTube
+# sleep 3m
+# sleep 40
+# ##Alan Walker - Faded
+# echo '''#Alan Walker - Faded'''
+# curl --data "v=60ItHLz5WEA" http://10.0.0.2:8008/apps/YouTube
+# sleep 3m
+# sleep 38
+# ##Christina Aguilera ft. P.Diddy - Tell Me (Suprafive Remix 2k15)(Music Video)
+# echo '''#Christina Aguilera ft. P.Diddy - Tell Me (Suprafive Remix 2k15)(Music Video)'''
+# curl --data "v=VEVPbzP9QKU" http://10.0.0.2:8008/apps/YouTube
+# sleep 4m
+# sleep 13
+# ##Duke Dumont - Ocean Drive
+# echo '''#Duke Dumont - Ocean Drive'''
+# curl --data "v=KDxJlW6cxRk" http://10.0.0.2:8008/apps/YouTube
+# sleep 3m
+# sleep 33
+# #Carly Rae Jepsen - I Really Like You (Dj Denis Rublev & Dj Anton Remix 2k16)
+# echo '''Carly Rae Jepsen - I Really Like You (Dj Denis Rublev & Dj Anton Remix 2k16)'''
+# curl --data "v=7UALo2Fkjfc" http://10.0.0.2:8008/apps/YouTube
+# sleep 4m
+# sleep 55
+# echo “Finished Playing List”
 
 
-cd /
+# cd /
